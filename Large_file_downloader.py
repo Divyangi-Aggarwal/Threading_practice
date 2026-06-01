891f01301e354992ba8fc3a1476ecb36315d2f46
Bhanu Aggarwal <divyangiaggarwal@gmail.com>
2026-06-01 12:16:11 +0530

    Update Large_file_downloader.py

--- Large_file_downloader.py
+++ Large_file_downloader.py
@@ -5,13 +5,12 @@ import requests
 from urllib.parse import urlparse
 import os
 
-def download_file(url, start, end, thread):
+def download_file(url, start, end, thread, extension ):
     print(f"Starting download from {url}")
     headers = {
         "Range": f"bytes={start}-{end}"
     }
-    path = urlparse(url).path
-    extension = os.path.splitext(path)[1]
+    
     response = requests.get(url, headers=headers, stream=True)
     print(
         f"Thread {thread}:",
@@ -20,13 +19,13 @@ def download_file(url, start, end, thread):
     with open(f"chunk_{thread}{extension}", 'wb') as f:
         f.write(response.content)
 
-def join_files(no_of_threads):
-    with open("largefile.pdf", 'wb') as output_file:
+def join_files(no_of_threads, extension):
+    with open(f"largefile{extension}", 'wb') as output_file:
         for i in range(no_of_threads):
-            with open(f"chunk_{i}.pdf", 'rb') as chunk_file:
+            with open(f"chunk_{i}{extension}", 'rb') as chunk_file:
                 output_file.write(chunk_file.read())
 
-def thread_creation(url, no_of_threads):
+def thread_creation(url, no_of_threads, extension ):
     threads = []
     response = requests.head(url)
     print(response.headers)
@@ -38,7 +37,7 @@ def thread_creation(url, no_of_threads):
     for thread in range(no_of_threads):
         start = thread * chunk_size
         end_chunk = (thread + 1) * chunk_size - 1
-        t = threading.Thread(target=download_file, args=(url, start, end_chunk, thread))
+        t = threading.Thread(target=download_file, args=(url, start, end_chunk, thread, extension))
         threads.append(t)
     return threads
 
@@ -47,12 +46,14 @@ def thread_creation(url, no_of_threads):
 def main():
     url = "http://localhost:8000/large_dummy.pdf"
     no_of_threads = 4
-    threads = thread_creation(url, no_of_threads)
+    path = urlparse(url).path
+    extension = os.path.splitext(path)[1]
+    threads = thread_creation(url, no_of_threads, extension)
     for t in threads:
         t.start()
     for t in threads:
         t.join()
-    join_files(no_of_threads)
+    join_files(no_of_threads, extension )
 
 if __name__ == "__main__":
     main()
